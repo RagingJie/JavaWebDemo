38dfc84ff204a2ce018817480d6aa536e9d898f7
naruto <2045933252@qq.com>
2025-05-16 14:23:41 +0800

    JavaWebDemo--【项目启动类】--添加注释

--- src/main/java/com/web/www/JavaWebApplication.java
+++ src/main/java/com/web/www/JavaWebApplication.java
@@ -1,17 +1,25 @@
 package com.web.www;
 
+import cn.dev33.satoken.SaManager;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableCaching // 开启缓存
 @EnableScheduling  // 开启定时任务
 @SpringBootApplication
 @MapperScan(basePackages = "com.web.www.mapper")
 public class JavaWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaWebApplication.class, args);
-	}
+    /**
+     * 启动项目
+     *
+     * @param args 参数
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebApplication.class, args);
+    }
 
 }
