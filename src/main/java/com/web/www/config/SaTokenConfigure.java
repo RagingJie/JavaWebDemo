76a52a2d8ed8fb319bd18ba4b66a44b586a6b768
naruto <2045933252@qq.com>
2025-05-22 11:04:01 +0800

    JavaWebDemo--【config】--注册 Sa-Token 拦截器，打开注解式鉴权功能

--- src/main/java/com/web/www/config/SaTokenConfigure.java
+++ src/main/java/com/web/www/config/SaTokenConfigure.java
@@ -1,14 +1,17 @@
 package com.web.www.config;
 
+import cn.dev33.satoken.interceptor.SaInterceptor;
 import cn.dev33.satoken.strategy.SaStrategy;
 import cn.dev33.satoken.util.SaFoxUtil;
 import cn.hutool.core.codec.Base64Encoder;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.PostConstruct;
 
 @Configuration
-public class SaTokenConfigure {
+public class SaTokenConfigure implements WebMvcConfigurer {
 
     /**
      * 重写 Sa-Token 框架内部算法策略
@@ -19,6 +22,15 @@ public class SaTokenConfigure {
         SaStrategy.instance.createToken = (loginId, loginType) -> {
             return Base64Encoder.encode(SaFoxUtil.getRandomString(64));    // 随机60位长度字符串
         };
+    }
+
+    // 注册 Sa-Token 拦截器，打开注解式鉴权功能
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 注册 Sa-Token 拦截器，打开注解式鉴权功能
+        registry.addInterceptor(new SaInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/sys/user/login", "/sys/user/isLogin");
     }
 
 }
