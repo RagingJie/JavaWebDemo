e1b5a01290cb7dc7463465334ef7d01a01cde154
naruto <2045933252@qq.com>
2025-05-23 14:44:59 +0800

    JavaWebDemo--【Constants】--系统常量

---
+++ src/main/java/com/web/www/commom/constants/SysConstants.java
@@ -0,0 +1,20 @@
+package com.web.www.commom.constants;
+
+/**
+ * 系统常量
+ *
+ * @author Naruto
+ * @since 2025-05-23
+ */
+public class SysConstants {
+
+    // 启用
+    public static final Integer ENABLE = 0;
+    // 禁用
+    public static final Integer DISABLE = 1;
+    // 已删除
+    public static final String DELETED = "y";
+    // 未删除
+    public static final String NOT_DELETED = "n";
+
+}
