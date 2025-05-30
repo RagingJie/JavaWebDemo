cd5bf417fba58fcf585d1d69b1272d39324a027c
naruto <2045933252@qq.com>
2025-05-30 14:50:55 +0800

    JavaWebDemo--【工具类】--ip工具类获取ip地址

---
+++ src/main/java/com/web/www/model/dto/IpAddressDto.java
@@ -0,0 +1,40 @@
+package com.web.www.model.dto;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class IpAddressDto implements Serializable {
+
+    private static final long serialVersionUID = -3892748923745L;
+
+    // 大洲
+    private String continent;
+
+    // 国家
+    private String country;
+
+    private String zipcode;
+
+    // 运营商
+    private String owner;
+
+    // 运营商
+    private String isp;
+
+    private String adcode;
+
+    // 省份
+    private String prov;
+
+    // 城市
+    private String city;
+
+    // 区县
+    private String district;
+}
